8cfb80eee431e6c717c1ee30f2d57597346867be
Mickey Puri <mickeypuri@hotmail.com>
2019-01-27 21:10:24 +0000

    added components for title, preferences, sorter, hotels to the App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
+import Title from "../title/title";
+import Preferences from "../preferences/preferences";
+import Sorter from "../sorter/sorter";
+import Hotels from "../hotels/hotels";
 
 const App = () => (
-  <div className="app-container" />
+  <div className="app-container">
+    <Title/>
+    <Preferences/>
+    <Sorter/>
+    <Hotels/>
+  </div>
 );
 
 export default App;
