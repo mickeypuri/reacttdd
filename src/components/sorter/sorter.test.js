c2419ff07ff35eec117c75061c77560aee0db9e6
Mickey Puri <mickeypuri@hotmail.com>
2019-01-27 21:47:35 +0000

    failing test, sorter should contain two button elements

--- src/components/sorter/sorter.test.js
+++ src/components/sorter/sorter.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Sorter from './hotels';
+import Preferences from "../preferences/preferences";
+import Preference from "../preference/preference";
 
 describe('Sorter', () => {
     it('should render a <div />', () => {
         const wrapper = shallow(<Sorter />);
         expect(wrapper.find('div').length).toEqual(1);
+    });
+
+    it ('should contain two button components', () => {
+        const wrapper = shallow (<Sorter />);
+        expect(wrapper.find('button').length).toEqual(2);
     });
 });
