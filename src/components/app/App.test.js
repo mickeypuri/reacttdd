8e29332d74b095cc3bf2409948daa599cd81477f
Mickey Puri <mickeypuri@hotmail.com>
2019-01-27 20:47:58 +0000

    added assertion that App contains a Title, Sorter, Preferences and Hotels

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -7,4 +7,24 @@ describe('App', () => {
         const wrapper = shallow(<App />);
         expect(wrapper.find('div').length).toEqual(1);
     });
+
+    it ('should contain a title component', () => {
+       const wrapper = shallow (<App />);
+        expect(wrapper.containsMatchingElement(<Title />)).toEqual(true);
+    });
+
+    it ('should contain a Sorter component', () => {
+        const wrapper = shallow (<App />);
+        expect(wrapper.containsMatchingElement(<Sorter />)).toEqual(true);
+    });
+
+    it ('should contain a Preferences component', () => {
+        const wrapper = shallow (<App />);
+        expect(wrapper.containsMatchingElement(<Preferences />)).toEqual(true);
+    });
+
+    it ('should contain a Hotels component', () => {
+        const wrapper = shallow (<App />);
+        expect(wrapper.containsMatchingElement(<Hotels />)).toEqual(true);
+    });
 });
