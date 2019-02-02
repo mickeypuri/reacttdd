d59e2abcfec8b547ae2ea4baafe1f5ce2672563e
Mickey Puri <mickeypuri@hotmail.com>
2019-02-02 15:02:58 +0000

    converted sorter to a class based component to allow it to hold state

--- src/components/sorter/sorter.js
+++ src/components/sorter/sorter.js
@@ -6,11 +6,17 @@ const Button = styled.button`
     width: 100px;
 `;
 
-const Sorter = () => (
-  <div className='sorterContainer'>
-      <Button className='sort-button'>Ascending</Button>
-      <Button className='sort-button'>Descending</Button>
-  </div>
-);
+class Sorter extends Component {
+
+    
+    render() {
+        return (
+            <div className='sorter-container'>
+                <Button className='sort-button'>Ascending</Button>
+                <Button className='sort-button'>Descending</Button>
+            </div>
+            );
+    }
+}
 
 export default Sorter;
