ebc8373b81aaf58f2e60b9f69d7d3372966ad856
Mickey Puri <mickeypuri@hotmail.com>
2019-02-03 18:47:56 +0000

    changed css to appear on a single row

--- src/actions/asyncCreators/fakeApiData.js
+++ src/actions/asyncCreators/fakeApiData.js
@@ -1,22 +1,22 @@
 const hotelData = [
     {
         name: 'Curzon',
-        imgUrl: 'https://via.placeholder.com/100',
+        imgUrl: 'https://via.placeholder.com/80',
         features:['pool','gymn','sauna']
     },
     {
         name: 'Plaza',
-        imgUrl: 'https://via.placeholder.com/100',
+        imgUrl: 'https://via.placeholder.com/80',
         features:['sports','restaurant','fine dining']
     },
     {
         name: 'Britannia',
-        imgUrl: 'https://via.placeholder.com/100',
+        imgUrl: 'https://via.placeholder.com/80',
         features:['business centre','concierge','views']
     },
     {
         name: 'Oberoi',
-        imgUrl: 'https://via.placeholder.com/100',
+        imgUrl: 'https://via.placeholder.com/80',
         features:['lift to airport','balconies','cordon bleu chef']
     }
 ];
--- src/components/hotels/hotel.js
+++ src/components/hotels/hotel.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import HotelTitle from './HotelTitle';
 import PropTypes from 'prop-types';
+import './hotel.css';
 
 const Hotel = ({hotel: {name, imgUrl, features, book}}) => (
   <div className="hotel-container">
