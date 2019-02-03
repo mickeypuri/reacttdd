776fd7b7d63f5d1da3bdb99a33a94b725dcdfa0d
Mickey Puri <mickeypuri@hotmail.com>
2019-02-03 14:49:09 +0000

    tidied up html

--- src/components/hotels/hotels.js
+++ src/components/hotels/hotels.js
@@ -4,11 +4,8 @@ import HotelTitle from './HotelTitle';
 const Hotels = () => (
   <div className="hotels-container">
       <HotelTitle className='hotel-title'/>
-
       <img className='hotel-image'></img>
-
       <ul className='hotel-features'></ul>
-
       <button className='hotel-book-now'>Book Now</button>
   </div>
 );
