c5181a7937fabef0fbce204aa89341b8f108b142
Mickey Puri <mickeypuri@hotmail.com>
2019-02-03 22:09:07 +0000

    installed json-server for fake rest Api

---
+++ src/fakeApi/server.js
@@ -0,0 +1,12 @@
+// server.js
+const jsonServer = require('json-server');
+const path = require('path');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(3004, () => {
+    console.log('JSON Server is running')
+});
